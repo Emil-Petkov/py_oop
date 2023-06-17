16a7cd4a595229bdc5276e585de303bc81c9707b
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-18 02:07:59 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,3 +9,20 @@ printing_list(first_list)
 print("second list")
 second_list = [8, 9, 10, None, 12, 13, 14]
 printing_list(second_list)
+
+# first list
+# 1
+# 2
+# 4
+# 5
+# 6
+# second list
+# 8
+# 9
+# 10
+# 12
+# 13
+# 14
+
+
+#########################################################################################
