0c5f66b87ec2822d3bfdc5a97fe471186e950bb8
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-18 02:04:07 +0300

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+def printing_list(list):
+    [print(x) for x in list if x is not None]
+
+
+print("first list")
+first_list = [1, 2, None, 4, 5, 6, None]
+printing_list(first_list)
+
+print("second list")
+second_list = [8, 9, 10, None, 12, 13, 14]
+printing_list(second_list)
