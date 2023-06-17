69cc8725836109eb6095fec237262445b10f0ab3
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-18 02:20:29 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -26,3 +26,47 @@ printing_list(second_list)
 
 
 #########################################################################################
+
+def get_name(numbers):
+    if numbers == 7:
+        return "seven"
+    elif numbers == 11:
+        return "eleven"
+    elif numbers == 5:
+        return "five"
+    else:
+        return numbers
+
+
+def printing_list(list):
+    for x in list:
+        if x is None:
+            continue
+        print(get_name(x))
+
+
+print("first list")
+first_list = [1, 2, None, 4, 5, 6, None, 7]
+printing_list(first_list)
+
+print("second list")
+second_list = [8, 9, 10, 11, None, 12, 13, 14]
+printing_list(second_list)
+
+# first list
+# 1
+# 2
+# 4
+# five
+# 6
+# seven
+# second list
+# 8
+# 9
+# 10
+# eleven
+# 12
+# 13
+# 14
+
+#################################################################################
