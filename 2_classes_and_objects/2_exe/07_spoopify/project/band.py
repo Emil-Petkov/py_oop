4be154469d7155281afbc8572190f212e3c8e1a4
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-29 08:53:20 +0300

    .

---
+++ 2_classes_and_objects/2_exe/07_spoopify/project/band.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.albums: List[str] = []
+
+    def add_album(self, album: str):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str):
+        for album in self.albums:
+            if album.name in album_name:
+                if album.published:
+                    return f"Album has been published. It cannot be removed."
+
+                self.albums.remove(album)
+                return f"Album {album_name} has been removed."
+
+        return f"Album {album_name} is not found."
+    def details(self):
+        result = f"Band {self.name}\n"
+        for album in self.albums:
+            result += album.details() + "\n"
+
+        return result.strip()
