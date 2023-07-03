4674e1b93971bc456fb055318662cccf352f7751
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-03 16:09:24 +0300

    Update delattr_method_demo.py

--- 4_encapsulation/demo/delattr_method_demo.py
+++ 4_encapsulation/demo/delattr_method_demo.py
@@ -8,3 +8,16 @@ p = Person("Emil")
 print(p.name)              # Emil
 print(delattr(p, "name"))  # None
 print(p.name)              # AttributeError: 'Person' object has no attribute 'name'
+
+
+################################################################
+
+class Phone:
+    def __delattr__(self, item):
+        del self.__dict__[item]
+        print(f"'{str(item)}' was deleted")
+
+
+phone = Phone()
+phone.color = "black"
+del phone.color        # 'color' was deleted
