9356d41f7861bc1bcd68587938409241586c4be0
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-03 14:27:28 +0300

    Update props_demo.py

--- 4_encapsulation/demo/props_demo.py
+++ 4_encapsulation/demo/props_demo.py
@@ -17,12 +17,34 @@ class Car:
 
 
 red_car = Car(200)
-red_car.drive()               # driving max speed 200
-red_car.max_speed = 512       # changes the speed to 447
-red_car.drive()               # driving max speed 447
+red_car.drive()  # driving max speed 200
+red_car.max_speed = 512  # changes the speed to 447
+red_car.drive()  # driving max speed 447
+
 
 # driving max speed 200
 # driving max speed 447
 
 
 ###################################################################
+
+class Person:
+    def __init__(self, name: str, age=0):
+        self.name = name
+        self.age = age
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 18:
+            self.__age = 18
+        else:
+            self.__age = value
+
+
+p = Person("Emil", 31)
+print(p.name)  # Emil
+print(p.age)   # 31
