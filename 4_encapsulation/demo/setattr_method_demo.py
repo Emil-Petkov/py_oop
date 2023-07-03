6211fbeae531e1d154fc5efcabadd9941a5908b0
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-03 15:58:36 +0300

    Update setattr_method_demo.py

--- 4_encapsulation/demo/setattr_method_demo.py
+++ 4_encapsulation/demo/setattr_method_demo.py
@@ -5,6 +5,19 @@ class Person:
 
 p = Person("Emil")
 print(setattr(p, "name", "Hello"))  # None
-print(setattr(p, "age", "Emil"))    # None
-print(setattr(p, "name", "Emil"))   # None
-print(p.name)                       # Emil
+print(setattr(p, "age", "Emil"))  # None
+print(setattr(p, "name", "Emil"))  # None
+print(p.name)  # Emil
+
+
+################################################################
+
+
+class Phone:
+    def __setattr__(self, attr, value):
+        self.__dict__[attr] = value.upper()
+
+
+phone = Phone()
+phone.color = "black"
+print(phone.color)   # BLACK
