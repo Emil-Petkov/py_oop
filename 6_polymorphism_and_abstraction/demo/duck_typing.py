45218b3ee364598b78e3c189748bdcd4b1357c50
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-11 15:25:17 +0300

    Create duck_typing.py

---
+++ 6_polymorphism_and_abstraction/demo/duck_typing.py
@@ -0,0 +1,15 @@
+class Cat:
+    def sound(self):
+        print("Meow!")
+
+
+class Train:
+    def sound(self):
+        print("Sound of wheels")
+
+
+for any_type in Cat(), Train():
+    any_type.sound()
+
+# Meow!
+# Sound of wheels
