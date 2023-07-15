e8096d6ee068b71ae3f3a159d1cdd7df2dde6acc
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-15 19:06:25 +0300

    Update 02_animal.py

--- 7_SOLID/1_lab/02_animal.py
+++ 7_SOLID/1_lab/02_animal.py
@@ -16,5 +16,52 @@
 #
 # animals = [Animal('cat'), Animal('dog')]
 # animal_sound(animals)
-
 ################################################################################
+
+
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+    def make_sound(self):
+        pass
+
+
+class Cat(Animal):
+
+    def __init__(self):
+        super().__init__("Cat")
+
+    def make_sound(self):
+        return "meow"
+
+
+class Dog(Animal):
+
+    def __init__(self):
+        super().__init__("Dog")
+
+    def make_sound(self):
+        return "woof-woof"
+
+
+class Chicken(Animal):
+
+    def __init__(self):
+        super().__init__("Chicken")
+
+    def make_sound(self):
+        return "cluck"
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
+
+animals = [Cat(), Dog(), Chicken()]
+animal_sound(animals)
+
+# meow
+# woof-woof
+# cluck
