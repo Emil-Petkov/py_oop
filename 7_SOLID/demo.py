9f6140510d5304be69fcb72e429eeffb0778a5ae
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-15 14:54:54 +0300

    Update demo.py

--- 7_SOLID/demo.py
+++ 7_SOLID/demo.py
@@ -1,3 +1,31 @@
+# class StudentTaxes:
+#     def __init__(self, name, semester_tax, average_grade):
+#         self.name = name
+#         self.semester_tax = semester_tax
+#         self.average_grade = average_grade
+#
+#     def get_discount(self):
+#         if self.average_grade > 5:
+#             return self.semester_tax * 0.4
+#         else:
+#             return self.semester_tax
+#
+#
+# class AdditionalDiscount(StudentTaxes):
+#     def get_discount(self):
+#         result = super().get_discount()
+#         if result:
+#             return result
+#         if 4 < self.average_grade <= 5:
+#             return self.semester_tax * 0.2
+#
+#
+# student = StudentTaxes("Student Name", 1000, 6)
+#
+# print(student.get_discount())  # 400.0
+
+########################################################################
+
 class StudentTaxes:
     def __init__(self, name, semester_tax, average_grade):
         self.name = name
@@ -7,27 +35,25 @@ class StudentTaxes:
     def get_discount(self):
         if self.average_grade > 5:
             return self.semester_tax * 0.4
-        else:
-            return self.semester_tax
+
+        return 0
 
 
 class AdditionalDiscount(StudentTaxes):
     def get_discount(self):
         result = super().get_discount()
-        if result:
+        if result > 0:
             return result
         if 4 < self.average_grade <= 5:
             return self.semester_tax * 0.2
 
+        return 0
 
-student = StudentTaxes("Student Name", 1000, 6)
-student2 = StudentTaxes("Student Name2", 1000, 3)
-student3 = StudentTaxes("Student Name3", 1000, 4)
 
-discount = student.get_discount()
-discount2 = student2.get_discount()
-discount3 = student3.get_discount()
+student = StudentTaxes("Emil", 1000, 6)
+student2 = StudentTaxes("Ivan", 1000, 5.5)
+student3 = StudentTaxes("Dragan", 1000, 5)
 
-print(discount) # 400.0
-print(discount2) # 1000
-print(discount3) # 1000
+print(student.get_discount())  # 400.0
+print(student2.get_discount())  # 400.0
+print(student3.get_discount())  # 0
