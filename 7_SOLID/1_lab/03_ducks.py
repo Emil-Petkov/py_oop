506c43cc6ab8afb1bc67ee24eff8fe4ba7d1a760
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-15 23:37:52 +0300

    Update 03_ducks.py

--- 7_SOLID/1_lab/03_ducks.py
+++ 7_SOLID/1_lab/03_ducks.py
@@ -57,3 +57,67 @@
 #         self.height = 0
 
 ################################################################
+
+from abc import abstractmethod, ABC
+
+
+class Duck(ABC):
+    @abstractmethod
+    def quack(self):
+        pass
+
+    @abstractmethod
+    def walk(self):
+        pass
+
+    @abstractmethod
+    def fly(self):
+        pass
+
+
+class RubberDuck(Duck):
+    def quack(self):
+        return "Squeek"
+
+    def walk(self):
+        """Rubber duck can walk only if you move it"""
+        raise NotImplementedError('I cannot walk by myself')
+
+    def fly(self):
+        """Rubber duck can fly only if you throw it"""
+        raise NotImplementedError('I cannot fly by myself')
+
+
+class RobotDuck(Duck):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    def quack(self):
+        return 'Robotic quacking'
+
+    def walk(self):
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
+
+
+rubber_duck = RubberDuck()
+print(rubber_duck.quack())  # Squeek
+print(rubber_duck.walk())  # NotImplementedError: I cannot walk by myself
+print(rubber_duck.fly())  # NotImplementedError: I cannot fly by myself
+
+robot_duck = RobotDuck()
+print(robot_duck.quack())  # Robotic quacking
+print(robot_duck.walk())  # Robotic walking
+print(robot_duck.fly())  # None
