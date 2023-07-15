eb25a8a13cb348e0f04d84cff514d60c74cef6bb
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-16 01:17:15 +0300

    Create demo_liskov_substitution.py

---
+++ 7_SOLID/demo_liskov_substitution.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def area(self):
+        return self.width ** 2
+
+
+def print_area(rectangle):
+    print(rectangle.area())
+
+
+r = Rectangle(2, 3)
+s = Square(2)
+
+print_area(r)  # 6
+print_area(s)  # 4
