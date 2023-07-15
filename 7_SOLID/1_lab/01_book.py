0e43eb80ea756168390d80aa83e15f86264d99e9
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-15 17:58:45 +0300

    Update 01_book.py

--- 7_SOLID/1_lab/01_book.py
+++ 7_SOLID/1_lab/01_book.py
@@ -9,3 +9,32 @@
 #         self.page = page
 
 ######################################################################
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.page = 0
+
+    def turn_page(self, page):
+        self.page = page
+
+
+class Libary:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def find_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        return None
+
+
+book = Book("Python for beginners", "Emil Petkov")
+
+print(book.title)  # Python for beginners
+print(book.author)  # Emil Petkov
