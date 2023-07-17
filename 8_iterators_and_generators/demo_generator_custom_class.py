74ba112da0c4e4a07e4d07f28b3efa89faa1488b
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-17 15:39:55 +0300

    Create demo_generator_custom_class.py

---
+++ 8_iterators_and_generators/demo_generator_custom_class.py
@@ -0,0 +1,40 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __iter__(self):
+        # yield ("name", self.name)
+        # yield ("age", self.age)
+        for pair in self.__dict__.items():
+            yield pair
+
+
+emil = Person("Emil", 101)
+
+for x in emil:
+    print(x)
+
+
+# ('name', 'Emil')
+# ('age', 101)
+
+#######################################################################
+
+def deep_loop(ll):
+    for value in ll:
+        for x in value:
+            yield x
+
+
+people = [
+    Person("Emil", 101),
+    Person("Ivan", 1)
+]
+
+[print(x) for x in deep_loop(people)]
+
+# ('name', 'Emil')
+# ('age', 101)
+# ('name', 'Ivan')
+# ('age', 1)
