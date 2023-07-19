47d493b94b4debfce2f19877ad04364d02c23643
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-19 13:11:26 +0300

    Create 09_possible_permutations.py

---
+++ 8_iterators_and_generators/2_exe/09_possible_permutations.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+
+def possible_permutations(elements):
+    for result in permutations(elements):
+        yield list(result)
+
+
+[print(n) for n in possible_permutations([1, 2, 3])]
+
+# [1, 2, 3]
+# [1, 3, 2]
+# [2, 1, 3]
+# [2, 3, 1]
+# [3, 1, 2]
+# [3, 2, 1]
