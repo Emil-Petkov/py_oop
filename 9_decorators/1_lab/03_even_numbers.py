574b4256824c4256b846dcff632a2adb3cc7ecd0
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-23 15:21:03 +0300

    Update 03_even_numbers.py

--- 9_decorators/1_lab/03_even_numbers.py
+++ 9_decorators/1_lab/03_even_numbers.py
@@ -1,12 +1,16 @@
 from functools import wraps
 
 
+def is_even(num):
+    return num % 2 == 0
+
+
 def even_numbers(function):
     @wraps(function)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args):
         result = function(*args)
 
-        return [num for num in result if num % 2 == 0]
+        return [num for num in result if is_even(num)]
 
     return wrapper
 
