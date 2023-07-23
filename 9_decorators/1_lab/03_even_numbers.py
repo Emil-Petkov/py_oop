25b3e78f2656b884b2c91b1078cc52936693f12b
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-23 14:40:56 +0300

    Create 03_even_numbers.py

---
+++ 9_decorators/1_lab/03_even_numbers.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+
+def even_numbers(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        result = function(*args, **kwargs)
+
+        return [num for num in result if num % 2 == 0]
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
+
+# [2, 4]
