e4255de4013eeac7f52eb4a2598c24991b927904
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-23 13:06:35 +0300

    Update demo.py

--- 9_decorators/demo.py
+++ 9_decorators/demo.py
@@ -1,6 +1,32 @@
+# def uppercase(func_to_decorate):
+#     result = func_to_decorate()
+#     return result.upper()
+#
+#
+# def get_shopping_list():
+#     return "Eggs, Milk, Bread"
+#
+#
+# def get_name():
+#     return "Emil Petkov"
+#
+#
+# print(uppercase(get_shopping_list))
+# print(uppercase(get_name))
+
+# EGGS, MILK, BREAD
+# EMIL PETKOV
+
+
+################################################################
+
+
 def uppercase(func_to_decorate):
-    result = func_to_decorate()
-    return result.upper()
+    def func_wrapper():
+        result = func_to_decorate()
+        return result.upper()
+
+    return func_wrapper
 
 
 def get_shopping_list():
@@ -11,8 +37,14 @@ def get_name():
     return "Emil Petkov"
 
 
-print(uppercase(get_shopping_list))
-print(uppercase(get_name))
+get_name = uppercase(get_name)
+get_shopping_list = uppercase(get_shopping_list)
 
-# EGGS, MILK, BREAD
-# EMIL PETKOV
+print(f"My name is {get_name()}")
+print(f"I have to buy: {get_shopping_list()}")
+
+
+
+
+
+################################################################
