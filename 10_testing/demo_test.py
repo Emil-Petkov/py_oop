e99697cb3b6bd26309b383f97a36b49330691307
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-29 11:28:28 +0300

    Update demo_test.py

--- 10_testing/demo_test.py
+++ 10_testing/demo_test.py
@@ -26,3 +26,14 @@ class MyFirstTestEver(TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+# (venv) hellhound@emil-VivoBook-ASUS:~/PycharmProjects/oop$ python 10_testing/demo_test.py
+# /home/hellhound/PycharmProjects/oop/10_testing/demo_test.py:10: DeprecationWarning: Please use assertEqual instead.
+#   self.assertEquals(1, 1)
+# ./home/hellhound/PycharmProjects/oop/10_testing/demo_test.py:20: DeprecationWarning: Please use assertNotEqual instead.
+#   self.assertNotEquals(1, 2)
+# .
+# ----------------------------------------------------------------------
+# Ran 2 tests in 0.000s
+#
+# OK
