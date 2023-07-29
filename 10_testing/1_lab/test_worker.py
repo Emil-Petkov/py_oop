693b0c84e58963c90c6bd91133fc752694ff6c7d
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-07-29 16:23:19 +0300

    Update test_worker.py

--- 10_testing/1_lab/test_worker.py
+++ 10_testing/1_lab/test_worker.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 class Worker:
     def __init__(self, name, salary, energy):
         self.name = name
@@ -31,12 +28,12 @@ class Worker:
 """
 
 from unittest import TestCase
+import unittest
 
 
 class WorkerTest(TestCase):
 
     def test_worker_is_initialized_correct(self):
-
         # Act
         worker = Worker("Emil", 2023, 60)
 
@@ -45,6 +42,76 @@ class WorkerTest(TestCase):
         self.assertEqual(2023, worker.salary)
         self.assertEqual(60, worker.energy)
         self.assertEqual(0, worker.money)
+
+    def test_worker_works(self):
+        # Arrange
+        worker = Worker("Emil", 2023, 60)
+        self.assertEqual(0, worker.money)
+        self.assertEqual(60, worker.energy)
+
+        # Act
+        worker.work()
+
+        # Assert
+        current_expected_money = 2023
+        self.assertEqual(current_expected_money, worker.money)
+        expected_energy = 60 - 1
+        self.assertEqual(expected_energy, worker.energy)
+
+        # Workers work again
+        worker.work()
+
+        current_expected_money = 2023 + 2023
+        self.assertEqual(current_expected_money, worker.money)
+        expected_energy = 59 - 1
+        self.assertEqual(expected_energy, worker.energy)
+
+    def test_worker_has_no_energy_can_not_work(self):
+        worker = Worker("Test Testov", 760, 0)
+
+        with self.assertRaises(Exception) as ex:
+            worker.work()
+
+        self.assertEqual("Not enough energy.", str(ex.exception.args[0]))
+
+    def test_worker_can_not_work_negative_energy(self):
+        worker = Worker("Test Testov", 760, -1)
+
+        with self.assertRaises(Exception) as ex:
+            worker.work()
+
+        self.assertEqual("Not enough energy.", str(ex.exception.args[0]))
+
+    def test_worker_energy_is_increase_when_worker_rests(self):
+        # Arrange
+        worker = Worker("Test Testov2", 760, 17)
+        self.assertEqual(17, worker.energy)
+
+        # Act
+        worker.rest()
+        self.assertEqual(18, worker.energy)
+
+        worker.rest()
+        self.assertEqual(19, worker.energy)
+
+    def test_get_info(self):
+        # Arrange
+        worker = Worker("Test Testov3", 999, 34)
+
+        # Act
+        result = worker.get_info()
+
+        # Assert
+        expected_result = 'Test Testov3 has saved 0 money.'
+        self.assertEqual(expected_result, result)
+
+        # Work again
+        worker.work()
+        result = worker.get_info()
+
+        # Assert
+        expected_result = 'Test Testov3 has saved 999 money.'
+        self.assertEqual(expected_result, result)
 
 
 if __name__ == '__main__':
